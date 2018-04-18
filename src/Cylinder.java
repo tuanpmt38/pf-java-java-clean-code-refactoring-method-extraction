50f17b813583180409f866ff6daccc8c917ad357
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-18 11:24:58 +0700

    pf-java clean code refactoring method extraction

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,16 +1,23 @@
 public class Cylinder {
-    public static double getVolume(int radius, int height){
-        double baseArea = getBaseArea(radius);
-        double perimeter = getPerimeter(radius);
-        double volume = perimeter * height + 2 * baseArea;
-        return volume;
-    }
+    //trước khi tách phương thức
+//    double baseArea = Math.PI * radius * radius;
+//    double perimeter = 2 * Math.PI * radius;
+//    double volume = perimeter * height + 2 * baseArea;
+//        return volume;
 
+    //sau khi tách phương thức
     private static double getPerimeter(int radius) {
-        return 2 * Math.PI  * radius;
+        return 2 * Math.PI * radius;
     }
 
     private static double getBaseArea(int radius) {
         return Math.PI * radius * radius;
+    }
+
+    public static double getVolume(int radius, int height) {
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        double volume = perimeter * height + 2 * baseArea;
+        return volume;
     }
 }
